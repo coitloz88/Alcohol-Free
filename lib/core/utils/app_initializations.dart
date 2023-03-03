1ae43a5302d97dacc8ca6e4fc232de34886d66d7
Hojung Shin <tankmom7279@gmail.com>
2023-03-03 13:28:00 +0900

    Feature/7 (#8)

--- lib/core/utils/app_initializations.dart
+++ lib/core/utils/app_initializations.dart
@@ -1,5 +1,7 @@
+import 'package:alcohol_free/app/data/providers/auth_provider.dart';
 import 'package:alcohol_free/core/values/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:get/get.dart';
 
 Future<void> initializeFirebase() async {
   await Firebase.initializeApp(
@@ -9,6 +11,7 @@ Future<void> initializeFirebase() async {
 
 Future<void> initializeProviders() async {
   // Provider 생성 시 추가
+  Get.put(AuthProvider());
 }
 
 Future<void> initializeServices() async {
---
+++ lib/core/values/strings.dart
@@ -0,0 +1,8 @@
+class AuthError {
+  static const INVALID_EMAIL = 'invalid-email';
+  static const USER_NOT_FOUND = 'user-not-found';
+  static const WRONG_PASSWORD = 'wrong-password';
+
+  static const EMAIL_ALREADY_IN_USE = 'email-already-in-use';
+  static const WEAK_PASSWORD = 'weak-password';
+}
