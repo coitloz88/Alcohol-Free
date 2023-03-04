03f8a1d6eaa9abb0fbefad1877e338dd2ff45e52
Hyemin Lee <cotloz88@gmail.com>
2023-03-05 01:26:55 +0900

    implement initial tab view

--- lib/core/languages/app_localizations_ko.dart
+++ lib/core/languages/app_localizations_ko.dart
@@ -6,4 +6,19 @@ class AppLocalizationsKo extends AppLocalizations {
 
   @override
   String get title => '알콜 프리';
+
+  @override
+  String get promise => '약속';
+
+  @override
+  String get diary => '일지';
+
+  @override
+  String get home => '홈';
+
+  @override
+  String get community => '커뮤니티';
+
+  @override
+  String get my => '마이';
 }
