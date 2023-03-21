055c87cb3cc50e284b7f5f95c51ed4520a541386
Hyemin Lee <cotloz88@gmail.com>
2023-03-22 02:02:21 +0900

    implement promise list item - card view 이용

--- lib/app/modules/promise/local_widgets/progress_list_item.dart
+++ lib/app/modules/promise/local_widgets/progress_list_item.dart
@@ -1,57 +1,121 @@
-import 'package:alcohol_free/app/data/models/progress_list_item_data.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:percent_indicator/linear_percent_indicator.dart';
 
 class ProgressListItemView extends StatelessWidget {
-  final PromiseProgressItem listItem;
-
-  const ProgressListItemView(this.listItem, {super.key});
+  const ProgressListItemView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Card(
-        color: Colors.white,
-        child: Container(
-            padding: const EdgeInsets.all(16.0),
-            height: 116,
-            width: double.infinity,
-            child: Column(
-              children: [
-                PromiseInfo(
-                    listItem.promiseName, listItem.startDate, listItem.endDate),
-                ProgressInfo(listItem.progress, listItem.profileImage),
+      clipBehavior: Clip.antiAliasWithSaveLayer,
+      color: const Color(0xFFFFF8EE),
+      elevation: 0,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: Padding(
+        padding: const EdgeInsetsDirectional.fromSTEB(16, 16, 16, 16),
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Row(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: const [
+                Text(
+                  '이거 제목 글자수 제한',
+                  style: TextStyle(fontWeight: FontWeight.w600),
+                ),
+                Align(
+                  alignment: AlignmentDirectional(0, 0),
+                  child: Text(
+                    '2022.02.23~2023.10.02',
+                    style: TextStyle(fontSize: 12),
+                  ),
+                ),
               ],
-            )));
-  }
-}
-
-class PromiseInfo extends StatelessWidget {
-  final String promiseName;
-  final String startDate;
-  final String endDate;
-
-  const PromiseInfo(this.promiseName, this.startDate, this.endDate,
-      {super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(children: [
-      SizedBox(width: 169, child: Text(promiseName)),
-      SizedBox(width: 128, child: Text('$startDate~$endDate'))
-    ]);
-  }
-}
-
-class ProgressInfo extends StatelessWidget {
-  final double progress;
-  final String profileImage;
-
-  const ProgressInfo(this.progress, this.profileImage, {super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return (ListTile(
-      trailing: Text(profileImage),
-      title: Text('progress: $progress/1.0'),
-    ));
+            ),
+            const SizedBox(
+              width: double.infinity,
+              height: 20,
+            ),
+            Row(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Column(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Align(
+                      alignment: AlignmentDirectional(-1, 0),
+                      child: Text(
+                        '주 1회만 하기',
+                        textAlign: TextAlign.start,
+                        style: TextStyle(fontSize: 12),
+                      ),
+                    ),
+                    LinearPercentIndicator(
+                      percent: 0.5,
+                      width: 177,
+                      lineHeight: 6,
+                      animation: false,
+                      progressColor: const Color(0xFFFFAC30),
+                      backgroundColor: const Color(0xFFF1F4F8),
+                      barRadius: const Radius.circular(5.5),
+                      padding: EdgeInsets.zero,
+                    ),
+                  ],
+                ),
+                Row(
+                  mainAxisSize: MainAxisSize.max,
+                  children: const [
+                    FaIcon(
+                      FontAwesomeIcons.handshakeAngle,
+                      color: Color(0xFFFFAC30),
+                      size: 12,
+                    ),
+                    Text(' 2', style: TextStyle(fontSize: 12)),
+                  ],
+                ),
+                Row(
+                  mainAxisSize: MainAxisSize.max,
+                  children: [
+                    Container(
+                      width: 44,
+                      height: 44,
+                      clipBehavior: Clip.antiAlias,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                      ),
+                      child: Image.network(
+                        'https://picsum.photos/seed/342/600',
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    Container(
+                      width: 44,
+                      height: 44,
+                      clipBehavior: Clip.antiAlias,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                      ),
+                      child: Image.asset(
+                        'assets/images/empty_profile.png',
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
