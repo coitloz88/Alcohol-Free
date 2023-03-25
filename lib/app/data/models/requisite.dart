e06567fd811bda18f8fb7954050c5749ec7fd433
hojungshin <tankmom7279@gmail.com>
2023-03-25 20:37:41 +0900

    조건 계산식 변경 및 ratio, iscompleted 정보도 firestore에 저장되도록 만듦

--- lib/app/data/models/requisite.dart
+++ lib/app/data/models/requisite.dart
@@ -2,28 +2,20 @@ import 'package:alcohol_free/app/data/enums/requisite_type.dart';
 import 'package:alcohol_free/app/data/models/consumption_based_requisite.dart';
 import 'package:alcohol_free/app/data/models/day_based_requisite.dart';
 import 'package:alcohol_free/app/data/models/journal.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 
 abstract class Requisite {
   bool isCompleted = false;
   double ratio = 0.0;
   DateTime from, to;
 
-  Requisite(this.from, this.to);
+  Requisite(this.from, this.to, this.ratio, this.isCompleted);
 
   factory Requisite.fromJson(json) {
     switch (RequisiteType.fromIndex(json['type'])) {
       case RequisiteType.consumptionBased:
-        return ConsumptionBased(
-          (json['from'] as Timestamp).toDate(),
-          (json['to'] as Timestamp).toDate(),
-          json['targetAlcoholConsumption'] as int,
-        );
+        return ConsumptionBased.fromJson(json);
       case RequisiteType.dayBased:
-        return DayBased(
-          (json['from'] as Timestamp).toDate(),
-          (json['to'] as Timestamp).toDate(),
-        );
+        return DayBased.fromJson(json);
     }
   }
 
