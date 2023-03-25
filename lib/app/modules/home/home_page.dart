08b1145bf4266f6506afd3f6d66d9679e79ca447
Hyemin Lee <cotloz88@gmail.com>
2023-03-26 02:42:31 +0900

    organize home page

--- lib/app/modules/home/home_page.dart
+++ lib/app/modules/home/home_page.dart
@@ -1,15 +1,25 @@
 import 'package:flutter/material.dart';
-import 'package:alcohol_free/app/modules/home/local_widgets/profile.dart';
-import 'package:alcohol_free/app/modules/home/local_widgets/week_calendar.dart';
+import 'local_widgets/profile_view.dart';
+import 'local_widgets/week_calendar_view.dart';
+import 'local_widgets/current_promise_view.dart';
 
 class HomePageView extends StatelessWidget {
   const HomePageView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-        mainAxisSize: MainAxisSize.max,
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: const [ProfileView(), WeekCalendarView()]);
+    return Padding(
+      padding: EdgeInsetsDirectional.fromSTEB(20, 0, 20, 0),
+      child: Column(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            const ProfileView(),
+            const SizedBox(height: 20),
+            const WeekCalendarView(),
+            const SizedBox(height: 30),
+            CurrentPromiseView(),
+          ]),
+    );
   }
 }
