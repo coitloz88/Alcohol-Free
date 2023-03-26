05ba5e8252b08d23295319480b32fce7e3083b63
Hyemin Lee <cotloz88@gmail.com>
2023-03-26 14:24:51 +0900

    wip

--- lib/app/modules/promise/local_widgets/promise_view.dart
+++ lib/app/modules/promise/local_widgets/promise_view.dart
@@ -1,39 +1,44 @@
 import 'package:alcohol_free/app/modules/promise/local_widgets/progress_list_item.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:get/get_state_manager/src/simple/get_state.dart';
 
+import '../../../data/models/promise.dart';
 import '../promise_page_controller.dart';
 
-class PromiseListView extends StatelessWidget {
+class PromiseListView extends StatefulWidget {
   final double listViewHeight;
-  const PromiseListView({Key? key, required this.listViewHeight})
-      : super(key: key);
+
+  PromiseListView({Key? key, required this.listViewHeight}) : super(key: key);
 
   @override
+  State<PromiseListView> createState() => _PromiseListViewState();
+}
+
+class _PromiseListViewState extends State<PromiseListView> {
+  @override
   Widget build(BuildContext context) {
-    return GetBuilder<PromisePageViewController>(
-        init: PromisePageViewController(),
-        builder: (controller) {
-          return Padding(
-              padding: const EdgeInsetsDirectional.fromSTEB(16, 16, 16, 16),
-              child: Column(mainAxisSize: MainAxisSize.min, children: [
-                const Align(
-                  alignment: AlignmentDirectional(-1, -1),
-                  child: Text(
-                    '진행중인 약속',
-                    style: TextStyle(fontWeight: FontWeight.w600),
-                  ),
-                ),
-                SizedBox(
-                    height: listViewHeight,
-                    child: ListView.builder(
-                        shrinkWrap: true,
-                        itemCount: controller.getPromiseList().length,
-                        itemBuilder: (context, index) {
-                          return ProgressListItemView(
-                              controller.getPromiseList()[index]);
-                        })),
-              ]));
-        });
+    return GetBuilder<PromisePageViewController>(builder: (controller) {
+      return Padding(
+          padding: const EdgeInsetsDirectional.fromSTEB(16, 16, 16, 16),
+          child: Column(mainAxisSize: MainAxisSize.min, children: [
+            const Align(
+              alignment: AlignmentDirectional(-1, -1),
+              child: Text(
+                '진행중인 약속',
+                style: TextStyle(fontWeight: FontWeight.w600),
+              ),
+            ),
+            SizedBox(
+                height: widget.listViewHeight,
+                child: ListView.builder(
+                    shrinkWrap: true,
+                    itemCount: controller.getPromiseList().length,
+                    itemBuilder: (context, index) {
+                      return ProgressListItemView(
+                          controller.getPromiseList()[index]);
+                    })),
+          ]));
+    });
   }
 }
