d9c06f95b472a5a455d90cc9744684aed521fea7
Hyemin Lee <cotloz88@gmail.com>
2023-03-26 20:30:54 +0900

    wip

--- lib/app/modules/drinking_journal_add/drinking_journal_page.dart
+++ lib/app/modules/drinking_journal_add/drinking_journal_page.dart
@@ -5,7 +5,35 @@ class DrinkingJournalPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return SingleChildScrollView(
+      child: Padding(
+          padding: EdgeInsetsDirectional.all(20),
+          child: Column(
+            children: [
+              //오늘의 기분은 어땠나요?
+            ],
+          )),
+    );
+  }
+}
+
+class ContainerWithHeading extends StatelessWidget {
+  final String title;
+  Container container;
+
+  ContainerWithHeading(
+      {super.key, required this.title, required this.container});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Text(title,
+            style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600)),
+        container,
+      ],
+    );
   }
 }
