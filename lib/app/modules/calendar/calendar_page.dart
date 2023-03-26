440677d17931d99d8ceb8340b10c62ee05677277
Hyemin Lee <cotloz88@gmail.com>
2023-03-26 15:45:16 +0900

    wip

---
+++ lib/app/modules/calendar/calendar_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/widgets.dart';
+
+class CalendarPage extends StatelessWidget {
+  const CalendarPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ();
+  }
+}
