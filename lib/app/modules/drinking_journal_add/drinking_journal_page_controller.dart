a0be0506107b2cb252492d53287a9a3e673cea84
Hyemin Lee <cotloz88@gmail.com>
2023-03-30 01:33:23 +0900

    implement drinking journal skeleton

--- lib/app/modules/drinking_journal_add/drinking_journal_page_controller.dart
+++ lib/app/modules/drinking_journal_add/drinking_journal_page_controller.dart
@@ -1,3 +1,27 @@
 import 'package:get/get.dart';
 
-class DrinkingJournalPageController extends GetxController {}
+class DrinkingJournalPageController extends GetxController {
+  int _numberOfPerson = 1;
+  final List<bool> _selectedDrinkState = <bool>[false, false, false];
+  int _numberOfAlcohol = 1;
+
+  int get numberOfPerson => _numberOfPerson;
+  List<bool> get selectedDrinkState => _selectedDrinkState;
+  int get numberOfAlcohol => _numberOfAlcohol;
+
+  void updateNumberOfPerson(int newNumber) {
+    _numberOfPerson = newNumber;
+    update();
+  }
+
+  void updateNumberOfAlcohol(int newNumber) {
+    _numberOfAlcohol = newNumber;
+    update();
+  }
+
+  void updateSelectedDrinkState(int index) {
+    for (int i = 0; i < _selectedDrinkState.length; i++) {
+      _selectedDrinkState[i] = i == index;
+    }
+  }
+}
