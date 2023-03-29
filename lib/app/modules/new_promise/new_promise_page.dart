fd3683d7d9b858b0677b5245ec7b279dd1a59e0f
Hyemin Lee <cotloz88@gmail.com>
2023-03-30 04:06:22 +0900

    implement new promise without date setting

---
+++ lib/app/modules/new_promise/new_promise_page.dart
@@ -0,0 +1,76 @@
+import 'package:alcohol_free/app/modules/new_promise/local_widgets/add_friend_button.dart';
+import 'package:alcohol_free/app/modules/new_promise/new_promise_page_controller.dart';
+import 'package:alcohol_free/app/widgets/confirm_button.dart';
+import 'package:alcohol_free/app/widgets/textbox_with_heading.dart';
+import 'package:alcohol_free/app/widgets/toggle_switch_with_heading.dart';
+import 'package:alcohol_free/app/widgets/widget_with_left_heading.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class NewPromisePage extends StatefulWidget {
+  const NewPromisePage({super.key});
+
+  @override
+  State<NewPromisePage> createState() => _NewPromisePageState();
+}
+
+class _NewPromisePageState extends State<NewPromisePage> {
+  final List<String> timeList =
+      List<int>.generate(24, (i) => i + 1).map((time) {
+    return time.toString();
+  }).toList();
+
+  @override
+  void initState() {
+    super.initState();
+
+    Get.put(NewPromisePageController());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GetBuilder<NewPromisePageController>(builder: (controller) {
+      return Scaffold(
+          appBar: AppBar(
+              title: Center(
+                  child: Text('목표 추가', style: TextStyle(color: Colors.black))),
+              backgroundColor: Colors.white,
+              leading: BackButton(
+                color: Colors.black,
+                onPressed: () {
+                  Get.back();
+                },
+              )),
+          body: SingleChildScrollView(
+            child: Padding(
+                padding: EdgeInsetsDirectional.all(20),
+                child: Column(
+                  children: [
+                    TextBoxWithHeading(
+                        heading: '목표 이름', height: 44, hint: '어떤 목표인지 알려주세요:)'),
+                    ToggleSwitchWithHeading(
+                        heading: '공개여부',
+                        initValue: controller.isPublic,
+                        handleToggle: (value) =>
+                            {controller.updateIsPublic(value)}),
+                    ToggleSwitchWithHeading(
+                        heading: '알람',
+                        initValue: controller.isNotification,
+                        handleToggle: (value) =>
+                            {controller.updateIsNotification(value)}),
+                    TextBoxWithHeading(heading: '메모', height: 70),
+                    Container(
+                        width: double.infinity,
+                        height: 12,
+                        color: Colors.white),
+                    Align(
+                        alignment: Alignment.centerLeft,
+                        child: WidgetWithLeftHeading(
+                            heading: '지킴이', childWidget: AddFriendButton())),
+                    ConfirmButton(buttonText: '시작하기')
+                  ],
+                )),
+          ));
+    });
+  }
+}
--- lib/app/modules/promise/promise_page_controller.dart
+++ lib/app/modules/promise/promise_page_controller.dart
@@ -2,7 +2,7 @@ import 'package:alcohol_free/app/data/models/promise.dart';
 import 'package:alcohol_free/app/data/services/promise_service/promise_service.dart';
 import 'package:get/get.dart';
 
-class PromisePageViewController extends GetxController {
+class PromisePageController extends GetxController {
   List<Promise> getPromiseList() {
     PromiseService _promiseService = PromiseService.to;
     return _promiseService.promiseList;
