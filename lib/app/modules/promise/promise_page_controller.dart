cd95e505bcae3bb68b9e0560726e35738e7b6beb
grace915 <grace0807@hanyang.ac.kr>
2023-03-31 03:26:00 +0900

    로그인 다시 원래대로, friends nullable로

--- lib/app/modules/promise/promise_page_controller.dart
+++ lib/app/modules/promise/promise_page_controller.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:alcohol_free/app/data/models/alcohol_free_user_friend.dart';
 import 'package:alcohol_free/app/data/models/promise.dart';
 import 'package:alcohol_free/app/data/services/promise_service/promise_service.dart';
@@ -5,6 +7,7 @@ import 'package:get/get.dart';
 
 class PromisePageController extends GetxController {
   List<Promise> getPromiseList(bool isAlone) {
+    log(PromiseService.to.promiseList.toString());
     if (isAlone)
       return PromiseService.to.getAlonePromiseList();
     else
