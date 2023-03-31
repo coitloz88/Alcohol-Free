6dc79da6a9a4ff59086863b1c9c4c6dd56da3b9d
grace915 <grace0807@hanyang.ac.kr>
2023-03-31 15:17:02 +0900

    기분 클릭 표시

--- lib/app/widgets/mood_selection_row.dart
+++ lib/app/widgets/mood_selection_row.dart
@@ -6,23 +6,32 @@ import 'package:get/get.dart';
 class MoodSelectionRow extends StatelessWidget {
   MoodSelectionRow(this.moodTypeWrapper, {Key? key}) : super(key: key);
   MoodTypeWrapper moodTypeWrapper;
+
   @override
   Widget build(BuildContext context) {
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Text(
           '오늘의 기분은 어땠나요?',
+          style: TextStyle(
+            fontSize: 20,
+            color: Colors.black,
+          ),
         ),
-        Row(
-          mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            MoodButton(MoodType.sad, moodTypeWrapper),
-            MoodButton(MoodType.angry, moodTypeWrapper),
-            MoodButton(MoodType.relaxed, moodTypeWrapper),
-            MoodButton(MoodType.happy, moodTypeWrapper),
-            MoodButton(MoodType.joyful, moodTypeWrapper),
-          ],
+        Container(
+          padding: EdgeInsets.only(top: 16),
+          child: Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              MoodButton(MoodType.sad, moodTypeWrapper),
+              MoodButton(MoodType.angry, moodTypeWrapper),
+              MoodButton(MoodType.relaxed, moodTypeWrapper),
+              MoodButton(MoodType.happy, moodTypeWrapper),
+              MoodButton(MoodType.joyful, moodTypeWrapper),
+            ],
+          ),
         )
       ],
     );
@@ -32,6 +41,7 @@ class MoodSelectionRow extends StatelessWidget {
 class MoodButton extends StatefulWidget {
   MoodButton(this.moodType, this.moodTypeWrapper, {Key? key}) : super(key: key);
   final MoodType moodType;
+  bool clicked = false;
   MoodTypeWrapper moodTypeWrapper;
 
   @override
@@ -45,10 +55,25 @@ class _MoodButtonState extends State<MoodButton> {
       onTap: () {
         widget.moodTypeWrapper.moodType = widget.moodType;
         print(widget.moodTypeWrapper.moodType);
+        setState(() {
+          widget.clicked = !widget.clicked;
+        });
       },
       child: Column(
         children: [
-          Image.asset('assets/images/${widget.moodType.name}.png'),
+          Stack(
+            children: [
+              Image.asset('assets/images/${widget.moodType.name}.png'),
+              Container(
+                height: 62,
+                width: 62,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(100),
+                  color: !widget.clicked ? Color(0x000000) : Color(0x55000000),
+                ),
+              )
+            ],
+          ),
           Text(widget.moodType.name)
         ],
       ),
